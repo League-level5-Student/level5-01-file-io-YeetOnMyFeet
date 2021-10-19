aacbd45b68b848444c9862db470c06dcccde4627
Roshan Sood <legodude2774@gmail.com>
2021-10-18 18:27:03 -0700

    Started level 5

--- src/_01_File_Recorder/FileRecorder.java
+++ src/_01_File_Recorder/FileRecorder.java
@@ -1,5 +1,14 @@
 package _01_File_Recorder;
 
+import java.util.Scanner;
+
 public class FileRecorder {
-	// Create a program that takes a message from the user and saves it to a file.
+	static // Create a program that takes a message from the user and saves it to a file.
+	Scanner scan = new Scanner(System.in);
+public static void main(String[] args) {
+	System.out.println("Ayo was poppin logang");
+	String input = scan.next();
+	System.out.println(input);
+	
+}
 }
